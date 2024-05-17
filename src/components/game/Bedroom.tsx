6c486dc1770f01871f87738501afd70930eb4f3a
wowalswjd <alswjd5491@naver.com>
2024-05-18 04:22:18 +0900

    :sparkles: 침실 아이패드 클릭 시 인벤토리 추가

--- src/components/game/Bedroom.tsx
+++ src/components/game/Bedroom.tsx
@@ -6,24 +6,40 @@ import pad from "../../assets/bedroom/pad.png";
 import tv_off from "../../assets/bedroom/tv_off.png";
 import Dialog from "../common/Dialog";
 import { bedroomData, bedroomKeys } from "../../data/bedroomData";
+import { useSetRecoilState } from "recoil";
+import { inventoryState } from "../../recoil/atom";
 
 // 침실 - 퍼즐 1
 const Bedroom = () => {
   const [activeDialog, setActiveDialog] = useState(false); // dialog 활성화 여부
   const [dialogScript, setDialogScript] = useState<string | null>(null); // 클릭한 아이템에 대한 대사
 
-  // 클릭 시 실행되는 함수
-  const onClickItem = (item: bedroomKeys) => {
+  const [showIpad, setShowIpad] = useState(true); // 아이패드 활성화 여부
+
+  const setInventory = useSetRecoilState(inventoryState); // 인벤토리 설정 함수
+
+  // 대화창 띄우기
+  const turnOnDialog = (item: bedroomKeys) => {
     setActiveDialog(true); // activeDialog를 true로 변경
     setDialogScript(bedroomData[item]); // 클릭한 item을 바탕으로 대사 찾기
   };
 
+  // 아이패드 클릭 시
+  const clickIpad = () => {
+    setShowIpad(false); // 아이패드 사라짐
+    setInventory((prev) => [
+      ...prev,
+      { id: prev.length, name: "pad", image: pad, checked: false },
+    ]); // 인벤토리에 아이패드 추가
+    turnOnDialog("pad"); // 아이패드 관련 대화창 활성화
+  };
+
   return (
     <Wrapper>
-      <Photo onClick={() => onClickItem("photo")} />
-      <Pillow onClick={() => onClickItem("pillow")} />
-      <Pad src={pad} alt="pad" />
-      <TV src={tv_off} alt="tv" onClick={() => onClickItem("tv")} />
+      <Photo onClick={() => turnOnDialog("photo")} />
+      <Pillow onClick={() => turnOnDialog("pillow")} />
+      {showIpad && <Pad src={pad} alt="pad" onClick={clickIpad} />}
+      <TV src={tv_off} alt="tv" onClick={() => turnOnDialog("tv")} />
       {activeDialog && (
         <Dialog setActiveDialog={setActiveDialog} dialogScript={dialogScript} />
       )}
--- src/data/bedroomData.ts
+++ src/data/bedroomData.ts
@@ -1,4 +1,6 @@
-export type bedroomKeys = "photo" | "pillow" | "tv";
+// 침실에서 사용할 데이터
+
+export type bedroomKeys = "photo" | "pillow" | "tv" | "pad";
 
 type bedroomTypes = {
   [key in bedroomKeys]: string;
@@ -9,4 +11,5 @@ export const bedroomData: bedroomTypes = {
     "할아버지와 어린 시절의 내가 함께 찍힌 사진이다. 동전 탑을 쌓으며 할아버지와 즐거운 시간을 보냈던 그 시절은 아직까지도 내게 가장 행복했던 기억으로 남아있다..",
   pillow: "리모콘을 발견했다…!",
   tv: "TV의 채널을 돌릴 무언가가 필요하다..",
+  pad: "아이패드를 발견했다…!",
 };
