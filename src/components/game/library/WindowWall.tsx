2099360f5375bd96f4897b5b451a205b7cbe1c1f
wowalswjd <alswjd5491@naver.com>
2024-06-08 17:59:29 +0900

    :lipstick: 창문벽 퍼블리싱

--- src/components/game/library/WindowWall.tsx
+++ src/components/game/library/WindowWall.tsx
@@ -1,10 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import bg_windowwall from "../../../assets/library/windowwall/bg_windowwall.png";
+import window_open from "../../../assets/library/windowwall/window_open.png";
+import window_close from "../../../assets/library/windowwall/window_close.png";
+import window_close_dark from "../../../assets/library/windowwall/window_close_dark.png";
+import bag from "../../../assets/library/windowwall/bag.png";
 
 // 창문벽
 const WindowWall = () => {
-  return <Wrapper></Wrapper>;
+  const [isWindowClose, setIsWindowClose] = useState(false); // 창문 닫혀 있는지 여부
+  const clickWindow = () => {
+    setIsWindowClose(!isWindowClose);
+  };
+  return (
+    <Wrapper>
+      <Lamp />
+      <Window
+        src={isWindowClose ? window_close : window_open}
+        alt="window_open"
+        onClick={clickWindow}
+      />
+      <Bag src={bag} />
+    </Wrapper>
+  );
 };
 
 export default WindowWall;
@@ -18,4 +36,30 @@ const Wrapper = styled.div`
   background-size: contain;
   overflow-y: hidden;
   position: relative;
+`;
+
+const Lamp = styled.div`
+  /* background-color: pink;
+  opacity: 0.4; */
+
+  position: absolute;
+  width: 7%;
+  height: 25%;
+  margin-left: 27.9%;
+  margin-top: 14%;
+`;
+
+const Window = styled.img`
+  position: absolute;
+  width: 30%;
+  margin-left: 35%;
+  margin-top: 4%;
+`;
+
+const Bag = styled.img`
+  position: absolute;
+  width: 12%;
+  bottom: 0;
+  margin-bottom: 3%;
+  margin-left: 31%;
 `;
