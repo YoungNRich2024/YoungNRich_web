ed2498444ee5e724c44ff6fd55fe0043ba99ec52
wowalswjd <alswjd5491@naver.com>
2024-06-10 02:56:32 +0900

    :lipstick: 금고벽 퍼블리싱

--- src/components/game/library/VaultWall.tsx
+++ src/components/game/library/VaultWall.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import styled from "styled-components";
 import bg_vaultwall from "../../../assets/library/vaultwall/bg_vaultwall.png";
+import vault_0sol from "../../../assets/library/vaultwall/vault_0sol.png";
+import frames from "../../../assets/library/vaultwall/frames.png";
 
 interface VaultWallProps {
   isDarkMode: boolean; // 불 껐는지 여부 (다크모드)
@@ -8,7 +10,12 @@ interface VaultWallProps {
 
 // 금고벽
 const VaultWall: React.FC<VaultWallProps> = ({ isDarkMode }) => {
-  return <Wrapper isDarkMode={isDarkMode}></Wrapper>;
+  return (
+    <Wrapper isDarkMode={isDarkMode}>
+      <Vault src={vault_0sol} alt="vault" isDarkMode={isDarkMode} />
+      <Frames src={frames} alt="frames" />
+    </Wrapper>
+  );
 };
 
 export default VaultWall;
@@ -25,4 +32,25 @@ const Wrapper = styled.div<{ isDarkMode: boolean }>`
 
   filter: ${(props) =>
     props.isDarkMode ? "brightness(0.5)" : "brightness(1)"};
+`;
+
+const Vault = styled.img<{ isDarkMode: boolean }>`
+  position: absolute;
+  width: 24.2%;
+  bottom: 0;
+  margin-bottom: 2.4%;
+  margin-left: 39.5%;
+
+  // 불 껐을 때 (다크모드) 밝기 올리기
+  filter: ${(props) =>
+    props.isDarkMode ? "brightness(1.5)" : "brightness(1)"};
+`;
+
+const Frames = styled.img`
+  position: absolute;
+  width: 24%;
+  right: 0;
+  margin-right: 12.3%;
+  /* margin-top: 16.5%; */
+  margin-top: 18%;
 `;
